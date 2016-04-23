6ff1bc9c3f12242de010bb1d56006fb00b75b6ea
Muhammad <qeymax@gmail.com>
2016-04-23 19:18:03 +0200

    Basic admin layout

--- aBlog/Areas/Admin/Controllers/UsersController.cs
+++ aBlog/Areas/Admin/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using aBlog.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,11 +8,12 @@ using System.Web.Mvc;
 namespace aBlog.Areas.Admin.Controllers
 {
     [Authorize(Roles = "admin")]
+    [SelectedTabAttribute("users")]
     public class UsersController : Controller
     {
         public ActionResult Index()
         {
-            return Content("users!!!!!!!");
+            return View();
         }
     }
 }
---
+++ aBlog/Infrastructure/SelectedTabAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aBlog.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class SelectedTabAttribute : ActionFilterAttribute
+    {
+        private readonly string _selectedtab;
+        public SelectedTabAttribute(string selectedtab)
+        {
+            _selectedtab = selectedtab;
+        }
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            filterContext.Controller.ViewBag.SelectedTab = _selectedtab;
+        }
+
+    }
+}
