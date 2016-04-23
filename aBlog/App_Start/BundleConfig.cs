9095c6a3cb7477c15e06493651e45fc627336578
Muhammad <qeymax@gmail.com>
2016-04-23 15:18:16 +0200

    Imported Styles and Scripts

---
+++ aBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace aBlog.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new StyleBundle("~/Admin/Styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/Admin.css"));
+
+            bundles.Add(new StyleBundle("~/Styles")
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/Site.css"));
+
+            bundles.Add(new ScriptBundle("~/Admin/Scripts")
+                .Include("~/Scripts/jquery-2.2.3.js")
+                .Include("~/Scripts/jquery.validate.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts")
+                .Include("~/Scripts/jquery-2.2.3.js")
+                .Include("~/Scripts/jquery.validate.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/Scripts/bootstrap.js"));
+        }
+    }
+}
